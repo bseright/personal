c6742b174c0e210f6a8c53286fd9b8d37687a7a5
Britain Seright <britainseright@gmail.com>
2023-02-28 20:22:37 -0600

    note out start of scroll animation

--- script.js
+++ script.js
@@ -171,7 +171,6 @@ createContent("p4");
 
 // animate nav bar on click and establish active class for scrolling animation
 let navButtons = document.querySelectorAll(".top-menu li");
-let sections = document.querySelectorAll(".section");
 let active = document.querySelector(".active");
 
 navButtons.forEach(item => {
@@ -206,3 +205,33 @@ let clickable = function() {
     currentDemo.style.pointerEvents = "all";
 }
 
+/*
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll(".section");
+let sectionID;
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionID = current.getAttribute("id");
+    
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".top-nav li [href*=" + sectionID + "]").classList.add("active");
+    } else {
+      document.querySelector(".top-nav li a[href*=" + sectionID + "]").classList.remove("active");
+    }
+  });
+}
+*/
