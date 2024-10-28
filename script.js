c9d54c7c1e6f2854aeeb716cdfb37d15a10e92fd
Britain Seright <51188389+bseright@users.noreply.github.com>
2024-10-28 15:09:33 -0500

    reduce master timer EVEN further (test)

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ let pMouseover = function() {
         setTimeout(() => {
             thisDiv.style.opacity = "100";
         }, "30")
-    }, "220")
+    }, "100")
 }
 
 let pMouseout = function() { 
